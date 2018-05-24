d7281d5c83158a4e3b82d0daeb8417a983f19cd1
m305-14\w401 <133@qq.com>
2018-05-24 16:08:33 +0800

    列表框

---
+++ 5_24/5_24/TodoItem.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5_24
+{
+    /// <summary>
+    /// TodoItem.xaml 的互動邏輯
+    /// </summary>
+    public partial class TodoItem : UserControl
+    {
+        public TodoItem()
+        {
+            InitializeComponent();
+        }
+        public bool IsChecked
+        {
+            set
+            {
+                if (value == true)
+                {
+                    CheckMark.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    CheckMark.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+        private void CheckBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (CheckMark.Visibility == Visibility)
+            {
+                CheckMark.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                CheckMark.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
